c0ef795e2f5aa0d8d07d06f96f129f87fcf6a443
ozysouza <ozysouzaa@gmail.com>
2024-09-09 10:50:54 -0400

    Create logic to upload image into CKEditor

---
+++ src/website/upload.py
@@ -0,0 +1,45 @@
+from flask import request, jsonify, Blueprint, url_for, current_app
+from werkzeug.utils import secure_filename
+import os
+
+from src.website.auth import logger
+
+upload = Blueprint('upload', __name__)
+
+
+@upload.route('/upload-image', methods=['GET', 'POST'])
+def upload_image():
+    if 'upload' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['upload']
+
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+
+        # Ensure the upload folder exists
+        upload_folder = current_app.config['UPLOAD_FOLDER']
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+            logger.info(f'Folder created: {upload_folder}')
+
+        file_path = os.path.join(upload_folder, filename)
+        try:
+            file.save(file_path)
+            file_url = url_for('static', filename=f'uploads/{filename}', _external=True)
+            logger.info(f'Image added into: {file_url}')
+
+            return jsonify({"url": file_url})
+        except Exception as err:
+            logger.error(f"Error: {err}")
+            return jsonify({"error": str(err)}), 500
+    else:
+        return jsonify({"error": "Invalid file type"}), 400
+
+
+def allowed_file(filename):
+    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
